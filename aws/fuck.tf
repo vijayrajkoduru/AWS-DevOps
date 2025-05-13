d32a4544653e1dadf6afb8215b905ab19d95c0d0
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-05-13 17:22:28 +0530

    again checking

--- aws/fuck.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "imported" {
-    instance_type = "t3.micro"
-    ami = "ami-09c813fb71547fc4f"
-    tags = {
-        "Name": "terraform-demo-changed",
-        "Purpose": "terraform-practice"
-    }
-}
