91c6884032838fe9e83d316aaf699beda1b7a9a9
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-05-20 16:11:46 +0530

    again checking

--- Shell-Scripting/part1.sh
+++ Shell-Scripting/part1.sh
@@ -1,52 +1,10 @@
 
 #!/bin/bash
 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+PERSON1=Ramesh # no space before and after equal
+PERSON2=Suresh
 
-LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1 )
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 ... $R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
-}
-
-echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
-
-if [ $USERID -ne 0 ]
-then
-    echo "ERROR:: You must have sudo access to execute this script"
-    exit 1 #other than 0
-fi
-
-dnf list installed mysql &>>$LOG_FILE_NAME
-
-if [ $? -ne 0 ]
-then # not installed
-    dnf install mysql -y &>>$LOG_FILE_NAME
-    VALIDATE $? "Installing MySQL"
-else
-    echo -e "MySQL is already ... $Y INSTALLED $N"
-fi
-
-
-dnf list installed git &>>$LOG_FILE_NAME
-
-if [ $? -ne 0 ]
-then
-    dnf install git -y &>>$LOG_FILE_NAME
-    VALIDATE $? "Installing Git"
-else
-    echo -e "Git is already ... $Y INSTALLED $N"
-fi
+echo "$PERSON1:: Hi $PERSON2"
+echo "${PERSON2}:: Hello $PERSON1"
+echo "$PERSON1:: How are you doing?"
+echo "$PERSON2:: I am good. How are you?"
