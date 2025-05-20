02812cb8c2d677179ba706b1dce527d55dd81333
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-05-20 21:18:00 +0530

    expense

---
+++ Expense-Project/basic.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
+fi
