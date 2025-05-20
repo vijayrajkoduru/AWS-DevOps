cb891033db56f9baea92269396cbe272da19ec62
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-05-20 19:21:21 +0530

    add this file

---
+++ Shell-Scripting/1-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "vijay is tring to communicate with the world"
+echo "Kiran wan to communicate with the vijay"
+echo "the both vijay and kiran are trying to communicate with each other"
