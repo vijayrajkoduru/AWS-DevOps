7b9b85e97b625bb4428854c679b8ca28f176458b
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-05-20 20:12:23 +0530

    deleted

---
+++ Shell-script/2-Variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "vijay:: Hi kiran"
+echo "kiran:: Hello vijay"
+echo "vijay:: How are you doing?"
+echo "kiran:: I am good. How are you?"
---
+++ Shell-script/5-Variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter your username::" # text entered here will be added as value to variable
+
+read -s USERNAME # Here USERNAME is the variable name
+
+echo "username entered: $USERNAME"
+
+echo "Please enter your password::"
+
+read -s PASSWORD
